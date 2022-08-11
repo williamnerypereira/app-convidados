bf9158bb525dc91cd63bd12b1ea200fb552e1231
William Nery <william.nery.pereira@gmail.com>
2022-08-11 11:19:29 -0300

    Modificações iniciais

--- app/src/main/java/com/williamnery/convidados/ui/AllGuestsViewModel.kt
+++ app/src/main/java/com/williamnery/convidados/ui/AllGuestsViewModel.kt
@@ -1,10 +1,10 @@
-package com.williamnery.convidados.ui.home
+package com.williamnery.convidados.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class AllGuestsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
--- app/src/main/java/com/williamnery/convidados/ui/PresetViewModel.kt
+++ app/src/main/java/com/williamnery/convidados/ui/PresetViewModel.kt
@@ -1,10 +1,10 @@
-package com.williamnery.convidados.ui.gallery
+package com.williamnery.convidados.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class PresetViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is gallery Fragment"
