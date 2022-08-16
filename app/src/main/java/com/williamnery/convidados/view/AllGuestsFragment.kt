78311cf0d732329a65037e6f592944068449ab7c
William Nery <william.nery.pereira@gmail.com>
2022-08-16 19:21:57 -0300

    Chamando listagem através da ViewModel

--- app/src/main/java/com/williamnery/convidados/view/AllGuestsFragment.kt
+++ app/src/main/java/com/williamnery/convidados/view/AllGuestsFragment.kt
@@ -13,31 +13,36 @@ import com.williamnery.convidados.viewmodel.AllGuestsViewModel
 class AllGuestsFragment : Fragment() {
 
     private var _binding: FragmentAllGuestsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var viewModel: AllGuestsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val viewModel =
+        viewModel =
             ViewModelProvider(this).get(AllGuestsViewModel::class.java)
 
         _binding = FragmentAllGuestsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        viewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        viewModel.getAll()
+
+        observe()
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun observe() {
+        viewModel.guests.observe(viewLifecycleOwner) {
+            // List de convidados
+
+        }
     }
 }
