98932d9469d465457b1b8af28548c01f0cf64867
William Nery <william.nery.pereira@gmail.com>
2022-09-08 11:39:35 -0300

    Camada de banco de dados

--- app/src/main/java/com/williamnery/convidados/repository/GuestDataBase.kt
+++ app/src/main/java/com/williamnery/convidados/repository/GuestDataBase.kt
@@ -3,11 +3,42 @@ package com.williamnery.convidados.repository
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.williamnery.convidados.constants.DataBaseConstants
+import com.williamnery.convidados.model.GuestModel
 
-class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+// class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+@Database(entities = [GuestModel::class], version = 1)
+abstract class GuestDataBase() : RoomDatabase() {
 
     companion object {
+        private lateinit var INSTANCE: GuestDataBase
+
+        fun getDataBase(context: Context): GuestDataBase {
+            if (!::INSTANCE.isAbstract) {
+                synchronized(GuestDataBase::class) {
+                    INSTANCE = Room.databaseBuilder(context, GuestDataBase::class.java, "guestdb")
+                        .addMigrations(MIGRATION_1_2)
+                        .allowMainThreadQueries()
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+
+        private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("DELETE FROM Guest")
+            }
+
+        }
+    }
+
+    /*companion object {
         private const val NAME = "guestdb"
         private const val VERSION = 1
     }
@@ -20,8 +51,6 @@ class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VE
                         DataBaseConstants.GUEST.COLUMNS.PRESENCE + " integer);")
     }
 
-    override fun onUpgrade(db: SQLiteDatabase, oldVerison: Int, newVerison: Int) {
-
-    }
+    override fun onUpgrade(db: SQLiteDatabase, oldVerison: Int, newVerison: Int) {}*/
 
 }
